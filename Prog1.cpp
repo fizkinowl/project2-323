0b5cf9dbe090625a8aa55a2bdace261375050f2d
WolferTamer <cbcassin@csu.fullerton.edu>
2024-04-19 13:13:01 -0700

    added some implementation for the driver

--- Prog1.cpp
+++ Prog1.cpp
@@ -4,10 +4,24 @@
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
+#include <stack>
 
 using namespace std;
 
+stack<char> pile;
+string input = "";
+int token = 0;
 
+char terminals[] = {'a','+','-','*','/','(',')'};
+
+bool isTerminal(char t) {
+    for(char i: terminals) {
+        if(t == i) {
+            return true;
+        }
+    }
+    return false;
+}
 
 void E() {
 
@@ -25,6 +39,33 @@ void F(){
 
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    pile.push('$');
+    pile.push('E');
+    for(int i = 1; i < argc; i++) {
+        input += argv[i];
+    }
+}
 
+bool driver() {
+    while(pile.top() != '$') {
+        char t = pile.top();
+        char i = input[token];
+        if(isTerminal(t)) {
+            if(t == i) {
+                pile.pop();
+                token++;
+            } else {
+                return false;
+            }
+        } else {
+            if(true) { //if table t,i has an entry then create that.
+                pile.pop();
+                pile.push('t'); //push the result of table t,i
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
 }
